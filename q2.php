8752ddd3da4d96ddc9523e0252bad1a4fb3601a8
purpledaisyz <130950675+purpledaisyz@users.noreply.github.com>
2025-11-12 11:06:40 +0800

    Create q2.php

---
+++ q2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q2</title>
+
+<style>
+  table, th, td {
+  border: 1px solid;
+}
+</style>
+
+</head>
+<body>
+<?php
+$students = [
+    [
+        'name' => 'Alice',
+        'program' => 'BIP',
+        'age' => 21
+    ],
+        
+    [
+        'name' => 'Bob',
+        'program' => 'BIS',
+        'age' => 20
+    ], 
+
+    [
+        'name' => 'Raju',
+        'program' => 'BIT',
+        'age' => 22
+    ]
+];
+?>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Program</th>
+        <th>Age</th> 
+    </tr>
+
+<?php
+    $no = 1;
+    foreach ($students as $student) {
+        echo "<tr>";
+        echo "<td>". $student['name']. "</td>";
+        echo "<td>". $student['program']. "</td>";
+        echo "<td>". $student['age']. "</td>";
+        echo "</tr>";
+
+    }
+?>
+   
+</table>
+
+</body>
+</html>
